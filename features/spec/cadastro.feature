c21f5cd034e1ae6e2766e22b35e3d9c0a746f06a
daniel <dani.ajala@yahoo.com>
2021-12-10 16:13:06 -0300

    teste api configuraçao

---
+++ features/spec/cadastro.feature
@@ -0,0 +1,12 @@
+#language: pt
+@cadastro
+Funcionalidade:Cadastro
+    Sendo um programador que possui projetos
+    Quero fazer o meu cadastro no github
+    Para que eu possa disponibilizar meus projetos
+    
+    Cenario: Fazer cadastro
+
+        Dado que acesso a página de cadastro
+        Quando submeto o meu cadastro completo
+        Então consigo ver meus trabalhos
---
+++ features/spec/cadastro_steps.rb
@@ -0,0 +1,12 @@
+Dado('que acesso a página de cadastro') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+  
+  Quando('submeto o meu cadastro completo') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('consigo ver meus trabalhos') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
